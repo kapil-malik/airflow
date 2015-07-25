f99d7a0d0e9acd5cba767e8de6158a0e382d34c0
Steven Yvinec-Kruyk <stevenyk@glassdoor.com>
2015-07-25 11:15:27 -0700

    Added SQL Server to connection admin drop down

--- airflow/www/app.py
+++ airflow/www/app.py
@@ -1607,6 +1607,7 @@ class ConnectionModelView(wwwutils.SuperUserMixin, AirflowModelView):
             ('s3', 'S3',),
             ('samba', 'Samba',),
             ('sqlite', 'Sqlite',),
+            ('mssql', 'Microsoft SQL Server'),
         ]
     }
 mv = ConnectionModelView(
